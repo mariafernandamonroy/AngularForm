b240ab3f2df40a2cbba9ac8221c608de1c773496
María Fernanda Monroy <maria.monroy@sofka.com.co>
2024-03-18 20:52:36 -0500

    encoding file

--- src/app/components/crypto/crypto.component.ts
+++ src/app/components/crypto/crypto.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import * as CryptoJS from 'crypto-js';
 @Component({
   selector: 'app-crypto',
   standalone: true,
   imports: [],
   templateUrl: './crypto.component.html',
-  styleUrl: './crypto.component.css'
+  styleUrl: './crypto.component.css',
 })
 export class CryptoComponent {
+  archivo: File | null = null;
+  checksum: string | null = null;
 
+  calcularChecksum() {
+    if (this.archivo) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const contenido = reader.result as string;
+        const hash = CryptoJS.SHA256(contenido).toString(CryptoJS.enc.Hex);
+        this.checksum = hash;
+      };
+      reader.readAsBinaryString(this.archivo);
+    }
+  }
+
+  manejarCambioArchivo(event: any) {
+    this.archivo = event.target.files[0];
+    console.log(this.calcularChecksum());
+  }
 }
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
+import { CryptoComponent } from '../crypto/crypto.component';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-form',
@@ -9,6 +11,8 @@ import Swal from 'sweetalert2';
   styleUrl: './form.component.css',
 })
 export class FormComponent {
+  // constructor(private cryptoComponent: CryptoComponent) {}
+
   [x: string]: any;
   showToast() {
     Swal.fire({
@@ -19,5 +23,23 @@ export class FormComponent {
       text: '¡La información se ha cargado con éxito!',
       icon: 'success',
     });
+  }
+  archivo: File | null = null;
+  checksum: string | null = null;
+
+  codificarArchivo(event: any) {
+    this.archivo = event.target.files[0];
+
+    if (this.archivo) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const contenido = reader.result as string;
+        const hash = CryptoJS.SHA256(contenido).toString(CryptoJS.enc.Hex);
+        this.checksum = hash;
+        console.log(this.checksum);
+      };
+      reader.readAsBinaryString(this.archivo);
+    }
   }
 }
